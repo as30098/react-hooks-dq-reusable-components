0ca163e4903cdff807f9831daec2d7b1471a8052
as30098 <alexspencer421@gmail.com>
2021-02-05 10:02:39 -0500

    attempted

--- src/components/PotionTile.js
+++ src/components/PotionTile.js
@@ -1,15 +1,35 @@
 import React from "react";
 
-/* 
+
+
+/*
 Write an abstracted component that can be used in place of InventoryTile AND ShopTile
 
-Consider what the tiles have in common and what they do not: 
-- For things that are the same, copy and pasting works just fine! 
-- For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
+Consider what the tiles have in common and what they do not:
+- For things that are the same, copy and pasting works just fine!
+- For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents
 */
 
 function PotionTile(props) {
-  return <div>PotionTile</div>;
+  return (
+    <div className={props.onClick.name === addToInventory ? "inventory-card" : "card"}>PotionTile</div>
+  );
 }
 
 export default PotionTile;
+
+
+// import React from "react";
+
+// function InventoryTile({ potion, sell, children }) {
+//   return (
+//     <div className="inventory-card">
+//       <div onClick={() => sell(potion.id)} className="image-wrapper">
+//         <img className="image" alt={potion.name} src={potion.image_url} />
+//       </div>
+//       {children}
+//     </div>
+//   );
+// }
+
+// export default InventoryTile;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,15 @@
 import React from "react";
-import ShopTile from "./ShopTile";
+import PotionTile from "./PotionTile";
 
 function Shop({ potions, addToInventory }) {
   const potionList = potions.map((potion) => {
     return (
-      <ShopTile key={potion.id} potion={potion} addToInventory={addToInventory}>
+      // <ShopTile key={potion.id} potion={potion} addToInventory={addToInventory}>
+      //   <h4 className="potion-header">{potion.name}</h4>
+      // </ShopTile>
+      <PotionTile key={potion.id} potion={potion} onClick={addToInventory}>
         <h4 className="potion-header">{potion.name}</h4>
-      </ShopTile>
+      </PotionTile>
     );
   });
 
